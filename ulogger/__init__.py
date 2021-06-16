a3491667424d5effacb91bc78b8f8a0e4b7d4420
Danielle Zephyr Malament <danielle.malament@gmail.com>
2021-06-16 19:19:49 -0400

    Bump to 2.0.1 to fix tag

--- ulogger/__init__.py
+++ ulogger/__init__.py
@@ -19,7 +19,7 @@ from __future__ import absolute_import
 from ulogger.ulogger import setup_logging
 
 __author__ = 'Lynn Root'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __license__ = 'Apache 2.0'
 __email__ = 'lynn@spotify.com'
 __description__ = 'Micro logging library'
